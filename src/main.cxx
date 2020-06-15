16d8c37f735f5d2be65c27be1e2625864d30dd2a
artemis-beta <krizar312@gmail.com>
2020-06-15 20:17:42 +0100

    Removed deprecated main

--- src/main.cxx
+++
@@ -1,8 +0,0 @@
-#include "TTBParser.hxx"
-
-int main()
-{
-    TTBParser::Parser parser;
-    parser.Parse("/Users/krizar/Documents/RailwayOpSimRepos/FR-ParisMetro/Program_Timetables/RATP_MetroMoFri.ttb");
-    return 0;
-}
